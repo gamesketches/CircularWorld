af4ac611687715c1a94b14deee4e1b7bc2a30873
Sam <sam.von.ehren@gmail.com>
2013-01-18 17:49:00 -0500

    Found some random color generating code, now every revolution generates a new flock of enemies

--- CircularWorld.js
+++ CircularWorld.js
@@ -3,7 +3,8 @@ window.onload = (function() {
 	var WIDTH = 900,
 		HEIGHT = 600,
 		ROLLBACK_POINT = 2100,
-		STARTING_POINT = 600;
+		STARTING_POINT = 600,
+		enemyColor = "#FF0000";
 
 	Crafty.init(WIDTH, HEIGHT);
 	 Crafty.background("#000000");
@@ -11,13 +12,36 @@ window.onload = (function() {
 	Crafty.e("Ground").attr({x: 0, y: 550, w: 3000, h: 50}).color("#888888");
 	var player = Crafty.e("Player").attr({x: STARTING_POINT, y: 300, w: 50, h: 50}).color("#0000FF");
 	Crafty.e("2D,Canvas,Color").attr({x: 1200, y: 500, w: 50, h: 50}).color("#00FF00");
-	Crafty.e("Enemy").attr({x: 900, y: 200, w: 50, h: 50}).color("#FF0000");
+	var enemies = [];
+	for(var i = 0; i < 8; i++) {
+		enemies[i] = Crafty.e("Enemy").attr({x: 900 + (10 * Crafty.math.randomInt(0, 20)), y: 200 + (10 * Crafty.math.randomInt(0,20)), w: 50, h: 50}).color(enemyColor);
+		}
+
+	function randomColor(min, max) {
+	    min = min !== undefined ? min.substr(1) : "000000";
+	    max = max !== undefined ? max.substr(1) : "FFFFFF";
+	    var c = [];
+                
+	    c[0] = Crafty.math.randomInt(parseInt(min.substr(0, 2), 16), parseInt(max.substr(0, 2), 16));
+	    c[1] = Crafty.math.randomInt(parseInt(min.substr(2, 2), 16), parseInt(max.substr(2, 2), 16));
+	    c[2] = Crafty.math.randomInt(parseInt(min.substr(4, 2), 16), parseInt(max.substr(4, 2), 16));
+        
+	    return 'rgb(' + c.join(',') + ')';
+	    }
 
 	Crafty.bind("EnterFrame", function() {
 		playerX = player.x;
 		playerY = player.y;
 		if(player.x >= ROLLBACK_POINT) {
-			player.x = STARTING_POINT;}
+			player.x = STARTING_POINT;
+			for(var i = 0; i < enemies.length; i++) {
+				enemies[i].destroy();
+				}
+			enemyColor = randomColor("#000000", "#FFFFFF");
+			for(var i = 0; i < 8; i++) {
+				enemies[i] = Crafty.e("Enemy").attr({x: 900 + (10 * Crafty.math.randomInt(0, 20)), y: 200 + (10 * Crafty.math.randomInt(0,20)), w: 50, h: 50}).color(enemyColor);
+				}
+			}
 		if(player.x <= 500) {
 			player.x = ROLLBACK_POINT; }
 		
