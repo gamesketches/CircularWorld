aa4ce6f5bced583fce6d6375b27e5646f1bce8e7
Sam <sam.von.ehren@gmail.com>
2013-01-18 17:29:20 -0500

    Added in enemy class that tracks player, and code for when the player gets hit

--- CircularWorld.js
+++ CircularWorld.js
@@ -11,9 +11,11 @@ window.onload = (function() {
 	Crafty.e("Ground").attr({x: 0, y: 550, w: 3000, h: 50}).color("#888888");
 	var player = Crafty.e("Player").attr({x: STARTING_POINT, y: 300, w: 50, h: 50}).color("#0000FF");
 	Crafty.e("2D,Canvas,Color").attr({x: 1200, y: 500, w: 50, h: 50}).color("#00FF00");
+	Crafty.e("Enemy").attr({x: 900, y: 200, w: 50, h: 50}).color("#FF0000");
 
 	Crafty.bind("EnterFrame", function() {
-
+		playerX = player.x;
+		playerY = player.y;
 		if(player.x >= ROLLBACK_POINT) {
 			player.x = STARTING_POINT;}
 		if(player.x <= 500) {
@@ -21,7 +23,7 @@ window.onload = (function() {
 		
 		var vpx = player.x - WIDTH/2;
 	
-		Crafty.viewport.x = -vpx
+		Crafty.viewport.x = -vpx;
 
 		});
 });
--- CircularWorldComponents.js
+++ CircularWorldComponents.js
@@ -1,8 +1,13 @@
+var playerX = 0, playerY = 0;
+
 Crafty.c("Player", {
 	init: function() {
 		this.addComponent("2D,Canvas,Color,Twoway,Collision,Gravity");
 		this.twoway(10,7);
 		this.collision();
+		this.onHit("Enemy", function() {
+			this.destroy(); // Go to game over later
+		});
 		this.gravity("Ground");
 		}
 	});
@@ -12,3 +17,21 @@ Crafty.c("Ground", {
 		this.addComponent("2D,Canvas,Color");
 		}
 	});
+
+Crafty.c("Enemy", {
+	init: function() {
+		this.addComponent("2D,Canvas,Color,Collision");
+		this.collision();
+		this.bind("EnterFrame", function() {
+			if(playerX > this.x)	{
+				this.x++; }
+			else {
+				this.x--; }
+			if(playerY > this.y) {
+				this.y++; }
+			else {
+				this.y--; }
+		});
+
+		}
+	});				
