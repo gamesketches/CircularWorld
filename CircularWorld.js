bfb83d50795792af0499cc02550587dcd8200d4d
Sam <sam.von.ehren@gmail.com>
2013-01-18 15:46:06 -0500

    The world is now circular. Added a generic 2D object so you can see it happening

--- CircularWorld.js
+++ CircularWorld.js
@@ -1,17 +1,27 @@
 window.onload = (function() {
 
-	var WIDTH = 1000,
-		HEIGHT = 600;
+	var WIDTH = 900,
+		HEIGHT = 600,
+		ROLLBACK_POINT = 2100,
+		STARTING_POINT = 600;
 
 	Crafty.init(WIDTH, HEIGHT);
 	 Crafty.background("#000000");
 
-	Crafty.e("Ground").attr({x: 0, y: 550, w: 1000, h: 50}).color("#888888");
-	var player = Crafty.e("Player").attr({x: 300, y: 300, w: 50, h: 50}).color("#0000FF");
+	Crafty.e("Ground").attr({x: 0, y: 550, w: 3000, h: 50}).color("#888888");
+	var player = Crafty.e("Player").attr({x: STARTING_POINT, y: 300, w: 50, h: 50}).color("#0000FF");
+	Crafty.e("2D,Canvas,Color").attr({x: 1200, y: 500, w: 50, h: 50}).color("#00FF00");
 
 	Crafty.bind("EnterFrame", function() {
+
+		if(player.x >= ROLLBACK_POINT) {
+			player.x = STARTING_POINT;}
+		if(player.x <= 500) {
+			player.x = ROLLBACK_POINT; }
+		
 		var vpx = player.x - WIDTH/2;
 	
 		Crafty.viewport.x = -vpx
+
 		});
 });
